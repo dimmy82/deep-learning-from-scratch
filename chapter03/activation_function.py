2b1e9baac73ae17587d026a5a9887b043711c176
teki.sei <teki.sei@uzabase.com>
2024-03-17 09:52:37 +0900

    ch03

--- chapter03/activation_function.py
+++ chapter03/activation_function.py
@@ -6,29 +6,35 @@ def step_function(x):
     return np.array(x > 0, dtype=np.int32)
 
 
-x1 = np.array([-1.0, 1.0, 2.0])
-print("x1: " + x1.__str__())
-print("step_function(x1): " + step_function(x1).__str__())
-
-x2 = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x2)
-plt.plot(x2, y)
-plt.ylim(-0.1, 1.1)
-# plt.show()
-
-
 def sigmoid_function(x):
     return 1 / (1 + np.exp(-x))
 
 
-x11 = np.array([-1.0, 1.0, 2.0])
-print("x11: " + x11.__str__())
-print("sigmoid(x11): " + sigmoid_function(x11).__str__())
+def identity_function(x):
+    return x
 
-x12 = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid_function(x12)
-plt.plot(x12, y)
-plt.ylim(-0.1, 1.1)
 
-plt.show()
+def run():
+    x1 = np.array([-1.0, 1.0, 2.0])
+    print("x1: " + x1.__str__())
+    print("step_function(x1): " + step_function(x1).__str__())
 
+    x2 = np.arange(-5.0, 5.0, 0.1)
+    y = step_function(x2)
+    plt.plot(x2, y)
+    plt.ylim(-0.1, 1.1)
+    # plt.show()
+
+    x11 = np.array([-1.0, 1.0, 2.0])
+    print("x11: " + x11.__str__())
+    print("sigmoid(x11): " + sigmoid_function(x11).__str__())
+
+    x12 = np.arange(-5.0, 5.0, 0.1)
+    y = sigmoid_function(x12)
+    plt.plot(x12, y)
+    plt.ylim(-0.1, 1.1)
+
+    plt.show()
+
+
+# run()
--- chapter03/neural_network.py
+++ chapter03/neural_network.py
@@ -1,15 +1,23 @@
 import numpy as np
+from activation_function import identity_function, sigmoid_function
 
 
-def neural_network(x1: np.int32, x2: np.int32) -> (np.int32, np.int32):
+def neural_network(x1: np.float32, x2: np.float32) -> (np.float32, np.float32):
     x = np.array([x1, x2])
-    w1 = np.array([[1, 2, 3], [4, 5, 6]])
-    layer1 = np.dot(x, w1)  # layer1.shape = (3,)
-    w2 = np.array([[7, 8], [9, 10], [11, 12]])
-    layer2 = np.dot(layer1, w2)  # layer2.shape = (2,)
-    w3 = np.array([[13, 14], [15, 16]])
-    layer3 = np.dot(layer2, w3)  # layer3.shape = (2,)
+    w1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    b1 = np.array([0.1, 0.2, 0.3])
+    layer1 = sigmoid_function(np.dot(x, w1) + b1)  # layer1.shape = (3,)
+    w2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    b2 = np.array([0.1, 0.2])
+    layer2 = sigmoid_function(np.dot(layer1, w2) + b2)  # layer2.shape = (2,)
+    w3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    b3 = np.array([0.1, 0.2])
+    layer3 = identity_function(np.dot(layer2, w3) + b3)  # layer3.shape = (2,)
     return (layer3[0], layer3[1])
 
 
-print("neural_network(1, 2) => " + str(neural_network(1, 2)))
+def run():
+    print("neural_network(1.0, 0.5) => " + str(neural_network(1.0, 0.5)))
+
+
+run()
